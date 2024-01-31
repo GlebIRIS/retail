6d844796e3c708b1467f8cc62a7cc023a3c7be69
GlebIRIS <150938750+GlebIRIS@users.noreply.github.com>
2024-01-31 16:50:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 from github import Github  # Install the 'github' library using: pip install PyGithub
-!pip install PyGithub
-from github import Github
 # GitHub repository details
 USERNAME = "GlebIRIS"
 REPO_NAME = "retail"
